692234402179843543c2c46f5735833a1e3e0cd5
Panven <58398356+Panven@users.noreply.github.com>
2021-01-06 11:51:30 +0100

    Java doc added

--- src/main/java/pl/put/poznan/transformer/logic/LowerTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/LowerTransformer.java
@@ -1,14 +1,43 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * Klasa sluzaca do transformacji tekstu do postaci malych liter
+ *
+ * 
+ */
+
 public class LowerTransformer extends Decorator {
+    
+     /**
+     * konstruktor tworzacy obiekt dekorujacy podany TextTransformer
+     *
+     *@param transformer typu TextTransformer
+     */
+    
     public LowerTransformer(InterfaceTextTransformer transformer){
         super(transformer);
     }
+    
+    /**
+     * Metoda transformujaca tekst do postaci malych liter
+     *
+     * @param text typu String przechowuje tekst majacy zostac poddany transformacji
+     * @return zwracany jest przetransformowany tekst
+     *
+     */
 
     private String lower(String text)
     {
         return text.toLowerCase();
     }
+    
+    /**
+     * Metoda przekazujaca aktualna modyfikacje textu do transformera, ktory nastepnie wykona metode lower
+     *
+     * @param text typu String przechowuje tekst majacy zostac poddany transformacji
+     * @return zwracane jest wywolanie metody lower
+     *
+     */
 
     public String transform(String text){
         return lower(transformer.transform(text));

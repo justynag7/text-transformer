e4ee663bb116126928cafb6833c80b09fc695fc5
Panven <58398356+Panven@users.noreply.github.com>
2021-01-06 11:54:37 +0100

    Java doc added
    first doc

--- src/main/java/pl/put/poznan/transformer/logic/UpperTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/UpperTransformer.java
@@ -1,14 +1,42 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * Klasa sluzaca do transformacji tekstu do postaci duzych liter
+ *
+ * 
+ */
+
 public class UpperTransformer extends Decorator{
+    
+    /**
+     * konstruktor tworzacy obiekt dekorujacy podany TextTransformer
+     *
+     *@param transformer typu TextTransformer
+     */
 
     public UpperTransformer(InterfaceTextTransformer transformer){
         super(transformer);
     }
+    
+    /**
+     * Metoda transformujaca tekst do postaci duzych liter
+     *
+     * @param text typu String przechowuje tekst majacy zostac poddany transformacji
+     * @return zwracany jest przetransformowany tekst
+     *
+     */
 
     private String upper(String text){
         return text.toUpperCase();
     }
+    
+    /**
+     * Metoda przekazujaca aktualna modyfikacje textu do transformera, ktory nastepnie wykona metode upper
+     *
+     * @param text typu String przechowuje tekst majacy zostac poddany transformacji
+     * @return zwracane jest wywolanie metody upper
+     *
+     */
 
     public String transform(String text){
         return upper(transformer.transform(text));

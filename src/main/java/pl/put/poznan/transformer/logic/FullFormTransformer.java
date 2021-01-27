9db34c589d22114734aa1d2603e6c51432ba44da
Panven <58398356+Panven@users.noreply.github.com>
2021-01-27 15:39:42 +0100

    Documentation code added

--- src/main/java/pl/put/poznan/transformer/logic/FullFormTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/FullFormTransformer.java
@@ -2,13 +2,31 @@ package pl.put.poznan.transformer.logic;
 
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+ * Klasa sluzaca do transformacji tekstu na format z rozwinietymi skrótami
+ *
+ *
+ */
 public class FullFormTransformer extends Decorator{
+    
+    /**
+     * konstruktor tworzacy obiekt dekorujacy podany TextTransformer
+     *
+     *@param transformer typu TextTransformer
+     */
 
     public FullFormTransformer(InterfaceTextTransformer transformer){
 
         super(transformer);
     }
+    
+    /**
+     * Metoda transformujaca tekst na format z rozwinietymi skrótami
+     *
+     * @param text typu String przechowuje tekst majacy zostac poddany transformacji
+     * @return zwracany jest przetransformowany tekst
+     *
+     */
 
     private String toFullForm(String text){
 
@@ -43,6 +61,14 @@ public class FullFormTransformer extends Decorator{
         }
         return text;
     }
+    
+    /**
+     * Metoda przekazujaca aktualna modyfikacje textu do transformera, ktory nastepnie wykona metode toFullForm
+     *
+     * @param text typu String przechowuje tekst majacy zostac poddany transformacji
+     * @return zwracane jest wywolanie metody toFullForm
+     *
+     */
 
     public String transform(String text){
         return toFullForm(transformer.transform(text));
